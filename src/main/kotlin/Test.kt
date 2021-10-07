4940bde7b3321eac6b84f5857f4b7ecaf068b7a5
ayan.j <bit.ayan8777@gmail.com>
2021-10-07 23:52:13 +0530

    Initial commit

---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,8 @@
+fun main(args:Array<String>) {
+    doPrint()
+    println("Build successful")
+}
+
+fun doPrint() {
+    println("This is executing")
+}
---
+++ src/main/kotlin/VarVal.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var message = "today is 10th october"
+    val toast = "opps ! Looks Like Something Went Wrong!"
+//    toast = "You Are Not Authorized!"
+
+    //in kotlin no need to define data type of the variables , program automatically detect the type of variable
+    // this feature is known as TYPE INFERENCE
+
+    //here we can define Variable by two Ways 1) Var 2) Val
+    // ** val means Const as like JAVA , if we assign some value into it , we can't Change The Value Anymore
+    // ** In Case of Var we can assign / reassign
+
+    print(toast)
+    print(message)
+}
